7b4aafa625365bdade2fabfee82fc8325468ac8d
Alfaz-Ahmad <120320595+Alfaz-Ahmad@users.noreply.github.com>
2024-01-14 18:50:30 +0000

    data types

---
+++ basics/data_type.js
@@ -0,0 +1,16 @@
+"use strict"; //for the newer versions of the js
+//Use MDN to written notes
+//ECMAScript is standard js
+
+let name = "Nikola Tesla";      //string 
+let year = 1856;                //number(range - 2^53)
+let alive = false;              //boolean
+
+//null -> value(representaion of empty value)
+//undefined -> not defined variable
+//symbol -> use in react and figma (to check uniquness)
+
+
+//object -> "-"
+
+//function typeof is used to find the data type of the variable given in the parameter
